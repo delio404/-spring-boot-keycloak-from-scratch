247cfb19fdf1d6a5956f57623610d8a402b8e9ba
delio404 <adelio.a.castro@gmail.com>
2023-05-29 22:01:19 -0300

    primerio push

---
+++ src/test/java/com/delios/springbootkeycloakfromscratch/SpringBootKeycloakFromScratchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.delios.springbootkeycloakfromscratch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootKeycloakFromScratchApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
